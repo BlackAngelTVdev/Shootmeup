4e71a65b67609a5355392c6b116bf16765a7b3bb
DJ Dam's <156111574+BlackAngelTVdev@users.noreply.github.com>
2025-10-08 15:12:40 +0200

    Feat(score) : ajout du score

---
+++ cowboy/Drones/Model/score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace CowBoy
+{
+
+
+    public partial class score
+    {
+        private static int _score = 0;
+
+        public int Score { get => _score; set => _score = value; }
+
+        public static void add()
+        {
+            _score++;
+            Console.WriteLine($"Score incrémenté : {_score}");
+        }
+    }
+
+
+}
---
+++ cowboy/Drones/View/score.cs
@@ -0,0 +1,31 @@
+﻿using CowBoy.Helpers;
+
+
+namespace CowBoy
+{
+    public partial class score
+    {
+        public void Render(BufferedGraphics drawingSpace)
+        {
+            Graphics g = drawingSpace.Graphics;
+
+            
+            Rectangle rect = new Rectangle(Sand.WIDTH - 160, 0, 150, 30);
+
+            
+            using (SolidBrush semiTransparentBrush = new SolidBrush(Color.FromArgb(128, Color.Gray)))
+            {
+                
+                g.FillRectangle(semiTransparentBrush, rect);
+            }
+
+            
+            g.DrawString($"{Score}", TextHelpers.drawFont, TextHelpers.writingBrush, rect.Left + 10, rect.Top + 5);
+        }
+
+
+
+
+    }
+}
+
