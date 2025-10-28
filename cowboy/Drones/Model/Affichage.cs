03641cfd3a16a37171faa92159709495b862d898
BlackAngelTVdev <156111574+BlackAngelTVdev@users.noreply.github.com>
2025-10-28 17:15:02 +0100

    Feat(Affichage Message) : la création la calss pour afficher des massage

---
+++ cowboy/Drones/Model/Affichage.cs
@@ -0,0 +1,13 @@
+﻿namespace CowBoy
+{
+    public partial class Affichage
+    {
+        private string _text = "";  // champ privé d’instance
+
+        public string Text
+        {
+            get => _text;
+            set => _text = value;
+        }
+    }
+}
---
+++ cowboy/Drones/View/Affichage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CowBoy.Helpers;
+
+namespace CowBoy
+{
+    public partial class Affichage
+    {
+        public static void Render(BufferedGraphics drawingSpace, string Text)
+        {
+            Graphics g = drawingSpace.Graphics;
+
+            Rectangle rect = new Rectangle(Sand.WIDTH / 2 - 100, Sand.HEIGHT / 2, 200, 30);
+
+            using (SolidBrush semiTransparentBrush = new SolidBrush(Color.FromArgb(128, Color.Gray)))
+            {
+                g.FillRectangle(semiTransparentBrush, rect);
+            }
+
+            StringFormat format = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            g.DrawString(Text, TextHelpers.drawFont, TextHelpers.writingBrush, rect, format);
+        }
+    }
+
+}
+
